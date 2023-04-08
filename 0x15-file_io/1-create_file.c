8fef0d80cd4a3b5646e7eb5db46ed0e24be7de37
AnoR <94970934+the4thmusketeerr@users.noreply.github.com>
2023-04-08 22:16:39 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,31 +3,31 @@
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @text: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text)
 {
-	int o, w, len = 0;
+    int file_descriptor, write_result, text_length = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    if (text != NULL)
+    {
+        for (text_length = 0; text[text_length];)
+            text_length++;
+    }
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+    file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    write_result = write(file_descriptor, text, text_length);
 
-	if (o == -1 || w == -1)
-		return (-1);
+    if (file_descriptor == -1 || write_result == -1)
+        return (-1);
 
-	close(o);
+    close(file_descriptor);
 
-	return (1);
+    return (1);
 }
